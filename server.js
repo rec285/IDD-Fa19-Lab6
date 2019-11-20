74a3d9da5672b30d21f0b179cc36d913590efa2d
Ryan Curtis <rec285@gmail.com>
2019-11-20 12:35:30 -0800

    update server

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var app = express(); // webapp
 var http = require('http').Server(app); // connects http library to server
 var io = require('socket.io')(http); // connect websocket library to server
 var serverPort = 8000;
+var SerialPort = require('serialport'); // serial library
 var router = express.Router();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
@@ -28,21 +29,26 @@ app.use('/api',router);
 http.listen(serverPort, function() {
   console.log('listening on *:%s', serverPort);
 });
+if (!process.argv[2]) {
+  console.error('Usage: node ' + process.argv[1] + ' SERIAL_PORT');
+  process.exit(1);
+}
+const serial = new SerialPort(process.argv[2], {});
 //----------------------------------------------------------------------------//
-
-
+console.log('c');
+serial.write('c');
 //---------------------- WEBSOCKET COMMUNICATION -----------------------------//
 // this is the websocket event handler and say if someone connects
 // as long as someone is connected, listen for messages
-io.on('connect', function(socket) {
-  console.log('a new user connected');
+//io.on('connect', function(socket) {
+//  console.log('a new user connected');
 
-  socket.on('message', (data) => { // If we get a new message from the client we process it;
-    console.log(data);
-  });
-  socket.on('disconnect', function() { // This function  gets called when the browser window gets closed
-    console.log('user disconnected');
-  });
-});
+//  socket.on('message', (data) => { // If we get a new message from the client we process it;
+//    console.log(data);
+//  });
+//  socket.on('disconnect', function() { // This function  gets called when the browser window gets closed
+//    console.log('user disconnected');
+//  });
+//});
 
 //----------------------------------------------------------------------------//
